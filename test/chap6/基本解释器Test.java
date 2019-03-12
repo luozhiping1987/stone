d67562ae59882ec4cb2fd3e96b9d5f9ef4b6beea
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-03-12 16:56:42 -0700

    润色

--- test/chap6/基本解释器Test.java
+++ test/chap6/基本解释器Test.java
@@ -14,16 +14,18 @@ import stone.Token;
 import stone.ast.ASTree;
 import stone.ast.NullStmnt;
 
+// 测试方式参考: https://github.com/chibash/gluonj/blob/fc453d9a187e60dfdaa1084a8974b8a4ba072aae/src/javassist/gluonj/util/UTester.java#L36
 public class 基本解释器Test extends BasicInterpreter {
 
-  private static String 初始化和 = "sum = 0";
-  private static String 初始化计数器 = "i = 1";
-  private static String 循环累加 =
+  private static final String 换行 = "\n";
+  private static final String 初始化和 = "sum = 0";
+  private static final String 初始化计数器 = "i = 1";
+  private static final String 循环累加 =
     "while i < 10 {\n" +
     "  sum = sum + i\n" +
     "  i = i + 1\n" +
     "}";
-  private static String 求值 = "sum";
+  private static final String 求值 = "sum";
 
   public static Object 求值(String 源代码) throws ParseException {
     return 求值(new BasicParser(), new BasicEnv(), 源代码);
@@ -42,12 +44,13 @@ public class 基本解释器Test extends BasicInterpreter {
   }
 
   @Test
-  public void test例程() throws Throwable {
-    if (UTester.runTestWith("chap6.BasicEvaluator")) return;
+  public void 例程() throws Throwable {
+    if (UTester.runTestWith("chap6.BasicEvaluator"))
+      return;
     assertEquals(0, 求值(初始化和));
-    assertEquals(1, 求值(初始化和 + "\n" + 初始化计数器));
-    assertEquals(10, 求值(初始化和 + "\n" + 初始化计数器 + "\n" + 循环累加));
-    assertEquals(45, 求值(初始化和 + "\n" + 初始化计数器 + "\n" + 循环累加 + "\n" + 求值));
+    assertEquals(1, 求值(初始化和 + 换行 + 初始化计数器));
+    assertEquals(10, 求值(初始化和 + 换行 + 初始化计数器 + 换行 + 循环累加));
+    assertEquals(45, 求值(初始化和 + 换行 + 初始化计数器 + 换行 + 循环累加 + 换行 + 求值));
   }
 
 }
