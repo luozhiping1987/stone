f8b447f9b4cfc671f1d2747c092774d24194ea36
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-03-14 15:05:34 -0700

    小重构

--- test/chap14/类型解释器.java
+++ test/chap14/类型解释器.java
@@ -3,25 +3,17 @@ package chap14;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
-import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
 
 import org.junit.Test;
 
-import chap11.EnvOptimizer;
 import chap11.ResizableArrayEnv;
-import chap6.BasicEvaluator;
-import chap6.Environment;
 import javassist.gluonj.util.UTester;
-import stone.BasicParser;
-import stone.Lexer;
 import stone.ParseException;
-import stone.Token;
 import stone.TypedParser;
-import stone.ast.ASTree;
-import stone.ast.NullStmnt;
 import stone.util.带类型返回值;
 import stone.util.文件功用;
+import stone.util.解释器功用;
 
 public class 类型解释器 extends TypedInterpreter {
 
@@ -40,25 +32,8 @@ public class 类型解释器 extends TypedInterpreter {
 
   public static 带类型返回值 求值(String 源代码) throws ParseException, TypeException {
     TypeEnv te = new TypeEnv();
-    return 求值(new TypedParser(),
+    return 解释器功用.带类型求值(new TypedParser(),
         new TypedNatives(te).environment(new ResizableArrayEnv()), te, 源代码);
-  }
-
-  public static 带类型返回值 求值(BasicParser bp, Environment env, TypeEnv typeEnv, String 源代码)
-      throws ParseException, TypeException {
-    Lexer lexer = new Lexer(new StringReader(源代码));
-    Object 终值 = null;
-    TypeInfo 类型 = null;
-    while (lexer.peek(0) != Token.EOF) {
-      ASTree tree = bp.parse(lexer);
-      if (!(tree instanceof NullStmnt)) {
-        ((EnvOptimizer.ASTreeOptEx) tree).lookup(((EnvOptimizer.EnvEx2) env).symbols());
-
-        类型 = ((TypeChecker.ASTreeTypeEx) tree).typeCheck(typeEnv);
-        终值 = ((BasicEvaluator.ASTreeEx) tree).eval(env);
-      }
-    }
-    return new 带类型返回值(终值, 类型);
   }
 
   @Test
--- test/stone/util/解释器功用.java
+++ test/stone/util/解释器功用.java
@@ -3,6 +3,10 @@ package stone.util;
 import java.io.StringReader;
 
 import chap11.EnvOptimizer;
+import chap14.TypeChecker;
+import chap14.TypeEnv;
+import chap14.TypeException;
+import chap14.TypeInfo;
 import chap6.BasicEvaluator;
 import chap6.Environment;
 import stone.BasicParser;
@@ -37,4 +41,22 @@ public class 解释器功用 {
     }
     return 终值;
   }
+
+  public static 带类型返回值 带类型求值(BasicParser 基本分析器, Environment 环境, TypeEnv 类型环境, String 源代码)
+      throws ParseException, TypeException {
+    Lexer 词法分析器 = new Lexer(new StringReader(源代码));
+    Object 终值 = null;
+    TypeInfo 类型 = null;
+    while (词法分析器.peek(0) != Token.EOF) {
+      ASTree 树 = 基本分析器.parse(词法分析器);
+      if (!(树 instanceof NullStmnt)) {
+        ((EnvOptimizer.ASTreeOptEx) 树).lookup(((EnvOptimizer.EnvEx2) 环境).symbols());
+
+        类型 = ((TypeChecker.ASTreeTypeEx) 树).typeCheck(类型环境);
+        终值 = ((BasicEvaluator.ASTreeEx) 树).eval(环境);
+      }
+    }
+    return new 带类型返回值(终值, 类型);
+  }
+
 }
