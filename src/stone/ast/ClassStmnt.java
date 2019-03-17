24c63e0c62cf75db63de421392d0794c9372e93f
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-03-17 09:30:58 -0700

    class->类别, extends->扩展

--- src/stone/ast/ClassStmnt.java
+++ src/stone/ast/ClassStmnt.java
@@ -2,6 +2,9 @@ package stone.ast;
 import java.util.List;
 
 public class ClassStmnt extends ASTList {
+  public static final String 关键字 = "类别";
+  public static final String 关键字_扩展 = "扩展";
+
     public ClassStmnt(List<语法树类> c) { super(c); }
     public String name() { return ((语法树叶类)子(0)).词().取文本(); }
     public String superClass() {
@@ -11,10 +14,11 @@ public class ClassStmnt extends ASTList {
             return ((语法树叶类)子(1)).词().取文本();
     }
     public ClassBody body() { return (ClassBody)子(子个数() - 1); }
+    @Override
     public String toString() {
         String parent = superClass();
         if (parent == null)
             parent = "*";
-        return "(class " + name() + " " + parent + " " + body() + ")";
+        return "(" + 关键字 + " " + name() + " " + parent + " " + body() + ")";
     }
 }
--- src/stone/类语法分析器类.java
+++ src/stone/类语法分析器类.java
@@ -1,5 +1,6 @@
 package stone;
 import static stone.Parser.rule;
+
 import stone.ast.ClassBody;
 import stone.ast.ClassStmnt;
 import stone.ast.Dot;
@@ -9,8 +10,8 @@ public class 类语法分析器类 extends 闭包语法分析器类 {
     Parser class_body = rule(ClassBody.class).sep("{").option(member)
                             .repeat(rule().sep(";", 词类.EOL).option(member))
                             .sep("}");
-    Parser defclass = rule(ClassStmnt.class).sep("class").identifier(reserved)
-                          .option(rule().sep("extends").identifier(reserved))
+    Parser defclass = rule(ClassStmnt.class).sep(ClassStmnt.关键字).identifier(reserved)
+                          .option(rule().sep(ClassStmnt.关键字_扩展).identifier(reserved))
                           .ast(class_body);
     public 类语法分析器类() {
         postfix.insertChoice(rule(Dot.class).sep(".").identifier(reserved));
